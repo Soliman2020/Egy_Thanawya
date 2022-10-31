926c03a9f64bad10b5ef23e163fb52c4654a9d85
Soliman <mohamed.h.hamed@gmail.com>
2022-10-31 12:46:09 +0200

    vital issue in official website related to not fixed locations for materials - only show the fixed ones

--- thanwya.py
+++ thanwya.py
@@ -51,144 +51,150 @@ geography_scores = []
 philosophy_scores = []
 psychology_scores = []
 total_scores = []
+def core():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        # > the target website
+        page.goto('https://g12.emis.gov.eg')
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    # > the target website
-    page.goto('https://g12.emis.gov.eg')
+        for seating_num in seating_nos:
+            page.is_visible('div.allcom')
+            time.sleep(1)
+            page.fill('input#SeatingNo',str(seating_num))
+            page.click('button[type=submit]')
 
-    for seating_num in seating_nos:
-        page.is_visible('div.allcom')
-        time.sleep(1)
-        page.fill('input#SeatingNo',str(seating_num))
-        page.click('button[type=submit]')
+            Ideal = page.is_hidden('div.text-center')
+            # print(Ideal)
 
-        Ideal = page.is_hidden('div.text-center')
-        # print(Ideal)
+            # > permit only valid seating numbers (no page error promot)
+            if Ideal == False:
+                continue
+            
+            page.is_visible('div.col-lg-12.justify-content-center.section-title')
 
-        # > permit only valid seating numbers (no page error promot)
-        if Ideal == False:
-            continue
+            # > read full html page
+            html = page.inner_html('html')
+
+            # > BeautifulSoup process
+            soup = BeautifulSoup(html,'html.parser')
+
+            # > student status (passed/failed)
+            status = soup.select('.animate__animated.animate__fadeInDown')
         
-        page.is_visible('div.col-lg-12.justify-content-center.section-title')
+            student_status = status[0].text
+            if student_status == 'ناجـح':
+                translated_status = 'Pass'
+            else:
+                translated_status = 'Fail'
+            assessment.append(translated_status)
+            # assessment.append(student_status)
 
-        # > read full html page
-        html = page.inner_html('html')
+            # > student data
+            student_details = soup.select('.p-data > table > tbody > tr td')
 
-        # > BeautifulSoup process
-        soup = BeautifulSoup(html,'html.parser')
+            desk_no = student_details[0].text
+            desk_nums.append(desk_no)
 
-        # > student status (passed/failed)
-        status = soup.select('.animate__animated.animate__fadeInDown')
-      
-        student_status = status[0].text
-        if student_status == 'ناجـح':
-            translated_status = 'Pass'
-        else:
-            translated_status = 'Fail'
-        assessment.append(translated_status)
-        # assessment.append(student_status)
+            # > hiding student name is better than showing
+            # student_name = student_details[1].text
+            # student_names.append(student_name)
 
-        # > student data
-        student_details = soup.select('.p-data > table > tbody > tr td')
+            school_name = student_details[2].text
+            school_names.append(school_name)
 
-        desk_no = student_details[0].text
-        desk_nums.append(desk_no)
+            governorate = student_details[3].text
+            governorates.append(governorate)
 
-        # > hiding student name is better than showing
-        # student_name = student_details[1].text
-        # student_names.append(student_name)
+            city = student_details[4].text
+            citys.append(city)
 
-        school_name = student_details[2].text
-        school_names.append(school_name)
+            # > student different materials
+            material = soup.select('.p-details > table > tbody > tr > th:nth-of-type(1)')
 
-        governorate = student_details[3].text
-        governorates.append(governorate)
+            mat4 = material[3].text
+            mat5 = material[4].text
+            mat6 = material[5].text
+            mat7 = material[6].text
 
-        city = student_details[4].text
-        citys.append(city)
+            # > student scores
+            scores = soup.select('.p-details > table > tbody > tr > th:nth-of-type(2)')
 
-        # > student different materials
-        material = soup.select('.p-details > table > tbody > tr > th:nth-of-type(1)')
+            Arabic = (scores[0].text)
+            Arabic_scores.append(Arabic)
 
-        mat4 = material[3].text
-        mat5 = material[4].text
-        mat6 = material[5].text
-        mat7 = material[6].text
+            F_1 = (scores[1].text)
+            F_1_scores.append(F_1)
 
-        # > student scores
-        scores = soup.select('.p-details > table > tbody > tr > th:nth-of-type(2)')
+            F_2 = (scores[2].text)
+            F_2_scores.append(F_2)
 
-        Arabic = float(scores[0].text)
-        Arabic_scores.append(Arabic)
+            # mat4_score = (scores[3].text)
+            # if mat4 == 'الأحياء':
+            #     biology_scores.append(mat4_score)
+            #     history_scores.append('NA')
+            # if mat4 == 'التاريخ':
+            #     history_scores.append(mat4_score)       
+            #     biology_scores.append('NA')
 
-        F_1 = float(scores[1].text)
-        F_1_scores.append(F_1)
+            # mat5_score = (scores[4].text)
+            # if mat5 == 'الجيولوجيا وعلوم البيئة':
+            #     geology_scores.append(mat5_score)
+            #     geography_scores.append('NA')
+            # if mat5 == 'الجغرافيا':
+            #     geography_scores.append(mat5_score)
+            #     geology_scores.append('NA')
 
-        F_2 = float(scores[2].text)
-        F_2_scores.append(F_2)
+            # mat6_score = (scores[5].text)
+            # if mat6 == 'الكيمياء':
+            #     chemistry_scores.append(mat6_score)
+            #     philosophy_scores.append('NA')
+            # if mat6 == 'الفلسفة والمنطق':
+            #     philosophy_scores.append(mat6_score)
+            #     chemistry_scores.append('NA')
 
-        mat4_score = float(scores[3].text)
-        if mat4 == 'الأحياء':
-            biology_scores.append(mat4_score)
-            history_scores.append('NA')
-        if mat4 == 'التاريخ':
-            history_scores.append(mat4_score)       
-            biology_scores.append('NA')
+            # mat7_score = (scores[6].text)
+            # if mat7 == 'الفيزياء':
+            #     physics_scores.append(mat7_score)
+            #     psychology_scores.append('NA')
+            # if mat7 == 'علم النفس والإجتماع':
+            #     psychology_scores.append(mat7_score)
+            #     physics_scores.append('NA')
 
-        mat5_score = float(scores[4].text)
-        if mat5 == 'الجيولوجيا وعلوم البيئة':
-            geology_scores.append(mat5_score)
-            geography_scores.append('NA')
-        if mat5 == 'الجغرافيا':
-            geography_scores.append(mat5_score)
-            geology_scores.append('NA')
+            total = (scores[7].text)
+            total_scores.append(total)
 
-        mat6_score = float(scores[5].text)
-        if mat6 == 'الكيمياء':
-            chemistry_scores.append(mat6_score)
-            philosophy_scores.append('NA')
-        if mat6 == 'الفلسفة والمنطق':
-            philosophy_scores.append(mat6_score)
-            chemistry_scores.append('NA')
+            # > To retart query for new desk number
+            page.go_back()
 
-        mat7_score = float(scores[6].text)
-        if mat7 == 'الفيزياء':
-            physics_scores.append(mat7_score)
-            psychology_scores.append('NA')
-        if mat7 == 'علم النفس والإجتماع':
-            psychology_scores.append(mat7_score)
-            physics_scores.append('NA')
+    natega_df = pd.DataFrame({'desk_no': desk_nums,
+                            # 'student_name':student_names,
+                            'school_name': school_names,
+                            'directorate': governorates,
+                            'neighborhood': citys,
+                            'arabic':Arabic_scores, 
+                            'first_forign_lang':F_1_scores, 
+                            'second_forign_lang':F_2_scores,
+                            # 'biology':biology_scores,
+                            # 'geology':geology_scores,
+                            # 'chemistry':chemistry_scores,
+                            # 'physics':physics_scores,
+                            # 'history':history_scores,
+                            # 'geography':geography_scores,
+                            # 'philosophy':philosophy_scores,
+                            # 'psychology':psychology_scores,
+                            'total_scores':total_scores,
+                            'status':assessment
+                            })
 
-        total = float(scores[7].text)
-        total_scores.append(total)
+    print(natega_df)
 
-        # > To retart query for new desk number
-        page.go_back()
+    natega_df.to_csv(f'natega_valid_range_{start}_to_{start+60}.csv',index=False)
 
-natega_df = pd.DataFrame({'desk_no': desk_nums,
-                        # 'student_name':student_names,
-                        'school_name': school_names,
-                        'directorate': governorates,
-                        'neighborhood': citys,
-                        'arabic':Arabic_scores, 
-                        'first_forign_lang':F_1_scores, 
-                        'second_forign_lang':F_2_scores,
-                        'biology':biology_scores,
-                        'geology':geology_scores,
-                        'chemistry':chemistry_scores,
-                        'physics':physics_scores,
-                        'history':history_scores,
-                        'geography':geography_scores,
-                        'philosophy':philosophy_scores,
-                        'psychology':psychology_scores,
-                        'total_scores':total_scores,
-                        'status':assessment
-                        })
+    print('Results are ready, check the output CSV file.')
 
-print(natega_df)
+def main():
+    core()
 
-natega_df.to_csv(f'natega_valid_range_{start}_to_{start+60}.csv',index=False)
-
-print('Results are ready, check the output CSV file.')
+if __name__ == '__main__':
+    main()
